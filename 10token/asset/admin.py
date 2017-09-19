749701c694e5eaeca703e4b42f320e2382a4016d
fortunto2 <info@3dstr.ru>
2017-09-20 02:58:45 +0500

    fix

--- 10token/asset/admin.py
+++ 10token/asset/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Asset
+from .models import Asset, Waves
 
+@admin.register(Asset)
 class AssetAdmin(admin.ModelAdmin):
     list_display = ('name','amount','token_id','created_date')
     list_filter = ('reissuable',)
     search_fields = ('name','amount','token_id','created_date')
 
-admin.site.register(Asset, AssetAdmin)
+
+@admin.register(Waves)
+class WavesAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Waves._meta.fields]
